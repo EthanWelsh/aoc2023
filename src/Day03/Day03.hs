770a75957e462f432822c78cf4f6ee9663d1ca8e
Ethan Welsh <ewelsh@google.com>
2023-12-16 11:49:46 -0800

    Use new Maze util lib for Day03.

--- src/Day03/Day03.hs
+++ src/Day03/Day03.hs
@@ -2,51 +2,26 @@ module Day03.Day03 (solve) where
 
 import Data.Char (isDigit)
 import Data.List (nub)
+import Utils.Maze
 
-type Board = [[Char]]
-
-type Point = (Int, Int)
-
-getPoint :: Board -> Point -> Char
-getPoint board (r, c) = board !! r !! c
+type Board = Maze Char
 
 isSymbol :: Char -> Bool
 isSymbol c = not (isDigit c) && not (c == '.')
 
-neighbours :: Board -> Point -> [Point]
-neighbours board (r, c) = filter (inBounds board) [(r, c - 1), (r, c + 1), (r - 1, c), (r + 1, c), (r - 1, c - 1), (r - 1, c + 1), (r + 1, c + 1), (r + 1, c - 1)]
-
 hasAdjacentSymbol :: Board -> Point -> Bool
 hasAdjacentSymbol board point =
-  let ns = neighbours board point
+  let ns = neighbors8 board point
       cs = map (getPoint board) ns
    in any (isSymbol) cs
-
-height :: Board -> Int
-height board = length board
-
-width :: Board -> Int
-width board = length (board !! 0)
-
-allPoints :: Board -> [Point]
-allPoints board = [(r, c) | r <- [0 .. (height board - 1)], c <- [0 .. (width board - 1)]]
-
-inBounds :: Board -> Point -> Bool
-inBounds board (r, c) = r >= 0 && r < (height board) && c >= 0 && c < (width board)
-
-left :: Point -> Point
-left (r, c) = (r, c - 1)
 
 findLeftStart :: Board -> Point -> Point
 findLeftStart board point = if isPointStart then point else findLeftStart board l
   where
-    l = left point
+    l = west point
     isOutOfBounds = not (inBounds board l)
     hasNoMoreDigits = not $ isDigit (getPoint board l)
     isPointStart = isOutOfBounds || hasNoMoreDigits
-
-testPoint :: Board -> (Char -> Bool) -> Point -> Bool
-testPoint board f p = f (getPoint board p)
 
 allDigitPoints :: Board -> [Point]
 allDigitPoints board = filter (testPoint board isDigit) (allPoints board)
@@ -61,7 +36,7 @@ getNumberFromOrigin board point =
 
 getNumbersAroundPoint :: Board -> Point -> [Int]
 getNumbersAroundPoint board point =
-  let ns = neighbours board point
+  let ns = neighbors8 board point
       numPoints = filter (testPoint board isDigit) ns
       origins = nub $ map (findLeftStart board) numPoints
    in map (getNumberFromOrigin board) origins
@@ -74,11 +49,11 @@ gearRatio board point = case (getNumbersAroundPoint board point) of
 solve :: FilePath -> IO ()
 solve filePath = do
   contents <- readFile filePath
-  let board = lines contents
+  let board = mazeFromList $ lines contents
   part1 board
   part2 board
 
-part1 :: [[Char]] -> IO ()
+part1 :: Board -> IO ()
 part1 board = do
   let digitPoints = allDigitPoints board
   let nextToSymbols = filter (hasAdjacentSymbol board) digitPoints
@@ -86,8 +61,8 @@ part1 board = do
   let nums = map (getNumberFromOrigin board) origins
   putStrLn $ "Part 1:" ++ (show (sum nums))
 
-part2 :: [[Char]] -> IO ()
+part2 :: Board -> IO ()
 part2 board = do
   let gearPoints = filter (testPoint board (== '*')) (allPoints board)
   let gearRatios = map (gearRatio board) gearPoints
-  putStrLn $ "Part 1:" ++ (show (sum gearRatios))
+  putStrLn $ "Part 2:" ++ (show (sum gearRatios))
