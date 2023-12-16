62b43f52002ea97b1268b05e0a299ff4fc2ce325
Ethan Welsh <ewelsh@google.com>
2023-12-15 21:16:08 -0800

    Add skeleton for days

---
+++ src/Day12/Day12.hs
@@ -0,0 +1,29 @@
+module Day12.Day12 (solve) where
+
+import Control.Monad (void)
+import ParserUtils (Parser, integer, charInRange)
+import Text.Megaparsec
+import Text.Megaparsec.Char (string, char, newline)
+
+type Input = String
+
+parseInput :: Parser Input
+parseInput = error "TODO"
+
+part1 :: Input -> IO ()
+part1 input = do
+  putStr $ "Part 1: "
+  print input
+
+part2 :: Input -> IO ()
+part2 input = do
+  putStr "Part 2: "
+
+solve :: FilePath -> IO ()
+solve filePath = do
+  contents <- readFile filePath
+  case parse parseInput filePath contents of
+          Left eb -> putStr (errorBundlePretty eb)
+          Right input -> do
+            part1 input
+            part2 input
--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,23 @@ import qualified Day05.Day05 as Day05
 import qualified Day06.Day06 as Day06
 import qualified Day07.Day07 as Day07
 import qualified Day08.Day08 as Day08
+import qualified Day09.Day09 as Day09
+import qualified Day10.Day10 as Day10
+import qualified Day11.Day11 as Day11
+import qualified Day12.Day12 as Day12
+import qualified Day13.Day13 as Day13
+import qualified Day14.Day14 as Day14
+import qualified Day15.Day15 as Day15
+import qualified Day16.Day16 as Day16
+import qualified Day17.Day17 as Day17
+import qualified Day18.Day18 as Day18
+import qualified Day19.Day19 as Day19
+import qualified Day20.Day20 as Day20
+import qualified Day21.Day21 as Day21
+import qualified Day22.Day22 as Day22
+import qualified Day23.Day23 as Day23
+import qualified Day24.Day24 as Day24
+import qualified Day25.Day25 as Day25
 
 import System.Environment (getArgs)
 
@@ -23,6 +40,23 @@ solvers =
     , Day06.solve
     , Day07.solve
     , Day08.solve
+    , Day09.solve
+    , Day10.solve
+	  , Day11.solve
+    , Day12.solve
+    , Day13.solve
+    , Day14.solve
+    , Day15.solve
+    , Day16.solve
+    , Day17.solve
+    , Day18.solve
+    , Day19.solve
+    , Day20.solve
+    , Day21.solve
+    , Day22.solve
+    , Day23.solve
+    , Day24.solve
+    , Day25.solve
     ]
 
 main :: IO ()
