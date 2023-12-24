031fc29cbd8b0ad9259770b6b9e0763327f99d21
Ethan Welsh <ewelsh@google.com>
2023-12-24 13:51:09 -0500

    Solve day 23 part 2.

--- src/Day23/Day23.hs
+++ src/Day23/Day23.hs
@@ -1,13 +1,15 @@
 module Day23.Day23 (solve) where
 
+import           Data.Function        (on)
+import           Data.List            (maximumBy, (\\))
+import           Data.List.Extra      (groupSort)
+import qualified Data.Map             as M
 import qualified Data.Set             as S
 import           ParserUtils          (Parser)
-import           Text.Megaparsec
-import           Text.Megaparsec.Char (char, newline, string)
+import           Text.Megaparsec      (choice, errorBundlePretty, many, parse,
+                                       sepBy)
+import           Text.Megaparsec.Char (char, newline)
 import           Utils.Maze
-import Data.List (maximumBy)
-import Data.Function (on)
-
 
 type Grid = Maze Char
 type Input = Grid
@@ -28,16 +30,13 @@ parseInput = do
 endPoint :: Grid -> Point
 endPoint m = (height m - 1, width m - 2)
 
-containsEnd :: Grid -> S.Set Point -> Bool
-containsEnd m visited = endPoint m `S.member` visited
-
 isEnd :: Grid -> Point -> Bool
 isEnd m p = p == (height m - 1, width m - 2)
 
 pruneMatching :: [a -> Bool] -> [a] -> [a]
 pruneMatching fs = filter (not . anyMatch fs)
   where
-    anyMatch fs x = any (\f -> f x) fs
+    anyMatch ffs x = any (\f -> f x) ffs
 
 step :: Grid -> S.Set Point -> Point -> [Point]
 step m visited p = let
@@ -51,6 +50,7 @@ step m visited p = let
     '>' -> [movePoint p East]
     'v' -> [movePoint p South]
     '<' -> [movePoint p West]
+    _   -> error "Unexpected tile encountered"
   in pruneMatching [isWall, isVisited, outOfBounds] ns
 
 longestPathPoints :: Grid -> S.Set Point -> Point -> S.Set Point
@@ -68,16 +68,60 @@ part1 m = do
   let pointsInPath = S.toList $ longestPathPoints m S.empty startPoint
   print $ length pointsInPath - 1
 
-printMaze :: Maze Char -> IO ()
-printMaze m = putStrLn $ unlines (mazeToList m)
+asMap :: Maze Char -> M.Map Point [Point]
+asMap m = let
+  ps = pruneMatching [isWall] (allPoints m)
+  isWall = testPoint m (=='#')
+  neighborsPerPoint = map (pruneMatching [isWall] . neighbors4 m) ps
+  in M.fromList (zip ps neighborsPerPoint)
+
+findNextIntersection :: M.Map Point [Point] -> Point -> Point -> (Point, Int)
+findNextIntersection m prev curr = let
+  ns = (m M.! curr) \\ [prev]
+  addOne (p, c) = (p, c + 1)
+  in case length ns of
+    0 -> (curr, 1)
+    1 -> addOne $ findNextIntersection m curr (head ns)
+    2 -> (curr, 1)
+    3 -> (curr, 1)
+    _ -> error "Unexpected length found"
+
+onlyIntersections :: M.Map Point [Point] -> [Point]
+onlyIntersections m = let
+  ns = M.keys m
+  isIntersection n = length (m M.! n) `elem` [1, 3, 4]
+  in filter isIntersection ns
+
+flatten :: (a, [b]) -> [(a, b)]
+flatten (a, bs) = map (a,) bs
+
+unflatten :: Ord a => [(a, b)] -> [(a, [b])]
+unflatten = groupSort
+
+simplifyGraph :: M.Map Point [Point] -> M.Map Point [(Point, Int)]
+simplifyGraph m = let
+  nodes = onlyIntersections m
+  edges = concatMap (flatten . (\n -> (n, m M.! n))) nodes :: [(Point, Point)]
+  simplifyEdge (curr, next) = (curr, findNextIntersection m curr next)
+  xxx = unflatten $ map simplifyEdge edges :: [(Point, [(Point, Int)])]
+  in M.fromList xxx
+
+longestPath :: M.Map Point [(Point, Int)] -> (Point -> Bool) -> [(Point, Int)] -> Point -> Int
+longestPath m atEnd steps p
+  | atEnd p = sum $ map snd steps
+  | otherwise = let
+    ns = filter (\(pp, _) -> pp `notElem` map fst steps) (m M.! p)
+    pathLens = map (\(pp, c) -> longestPath m atEnd ((pp, c):steps) pp) ns
+    in maximum (0:pathLens)
 
 part2 :: Input -> IO ()
 part2 m = do
   putStr "Part 2: "
   let startPoint = (0, 1)
+  let atEnd p = (endPoint m) == p
   let mm = setPointsMatching m (/='#') '.'
-  let pointsInPath = S.toList $ longestPathPoints mm S.empty startPoint
-  print $ length pointsInPath - 1
+  let g = simplifyGraph $ asMap mm
+  print $ longestPath g atEnd [((0, 1), 0)] startPoint
 
 solve :: FilePath -> IO ()
 solve filePath = do
