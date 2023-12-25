e20a51a97bf0dd210b281813a3861f42df10e5f4
Ethan Welsh <ewelsh@google.com>
2023-12-25 14:20:48 -0500

    Parsing for day 24.

--- src/Day24/Day24.hs
+++ src/Day24/Day24.hs
@@ -1,14 +1,55 @@
 module Day24.Day24 (solve) where
 
-import           ParserUtils     (Parser)
 import           Text.Megaparsec
---import Control.Monad (void)
---import Text.Megaparsec.Char (string, char, newline)
+import Control.Monad (void)
+import Text.Megaparsec.Char (string, char, newline)
+import ParserUtils
+import qualified Text.Megaparsec.Char.Lexer as L
 
-type Input = String
+newtype Position = Position (Int, Int, Int) deriving (Show, Eq, Ord)
+newtype Velocity = Velocity (Int, Int, Int) deriving (Show, Eq, Ord)
+data Hail = Hail Position Velocity deriving (Show, Eq, Ord)
+type Input = [Hail]
+
+--20, 19, 15 @ 1, -5, -3
+
+negativeInteger :: Parser Int
+negativeInteger = do
+  void $ char '-'
+  n <- integer
+  return (n * (-1))
+
+signedInteger :: Parser Int
+signedInteger = negativeInteger <|> integer
+
+manySpaces :: Parser ()
+manySpaces = void $ many (char ' ')
+
+commaAndSpaces :: Parser ()
+commaAndSpaces = do
+  void $ char ','
+  manySpaces
+
+parseXyz :: Parser (Int, Int, Int)
+parseXyz = do
+  x <- signedInteger
+  commaAndSpaces
+  y <- signedInteger
+  commaAndSpaces
+  z <- signedInteger
+  return (x, y, z)
+
+parseLine :: Parser Hail
+parseLine = do
+  p <- parseXyz
+  manySpaces
+  void $ string "@"
+  manySpaces
+  v <- parseXyz
+  return (Hail (Position p) (Velocity v))
 
 parseInput :: Parser Input
-parseInput = error "TODO"
+parseInput = many parseLine
 
 part1 :: Input -> IO ()
 part1 input = do
